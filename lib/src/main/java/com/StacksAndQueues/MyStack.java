a1e489b279290bbdea691870ed498f02f5f3617c
mohdarshad1 <mdarshad181@gmail.com>
2021-05-05 23:07:21 +0530

    [Mohd Arshad] : Add Peak and Pop from Stack

--- lib/src/main/java/com/StacksAndQueues/MyStack.java
+++ lib/src/main/java/com/StacksAndQueues/MyStack.java
@@ -1,18 +1,20 @@
 package com.StacksAndQueues;
 
-public class MyStack<K extends Comparable<K>>{
+public class MyStack<K extends Comparable<K>> {
 	private final MyLinkedList<K> myLinkedList;
-	
 	public MyStack() {
 		myLinkedList = new MyLinkedList<>();
 	}
 	public void push(INode<K> element) {
 		myLinkedList.add(element);
 	}
-	public INode<K> peak(){
+	public INode<K> peak() {
 		return myLinkedList.head;
 	}
 	public void printStack() {
 		myLinkedList.printMyNode();
+	}
+	public INode<K> pop() {
+		return myLinkedList.pop();
 	}
 }
--- lib/src/test/java/com/StacksAndQueues/MyStackTest.java
+++ lib/src/test/java/com/StacksAndQueues/MyStackTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class MyStackTest {
 	
 	@Test
-	public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode() {
+	public void given3Numbers_AddedToStack_ShouldHaveLastAddedNode() {
 		MyStack<Integer> myStack=new MyStack<>();
 		MyNode<Integer> myFirstNode = new MyNode<>(70);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
@@ -17,5 +17,27 @@ public class MyStackTest {
 		INode<Integer> myNode=myStack.peak();
 		myStack.printStack();
 		assertEquals(myThirdNode,myNode);
+	}
+	
+	@Test
+	public void given3Numbers_PoppedFromStack_TillListIsEmpty() {
+		MyStack<Integer> myStack = new MyStack<>();
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		INode<Integer> popedNode1 = myStack.pop();
+		myStack.printStack();
+		assertEquals(myThirdNode, popedNode1);
+		INode<Integer> myNode = myStack.peak();
+		myStack.printStack();
+		assertEquals(mySecondNode, myNode);
+		INode<Integer> popedNode2 = myStack.pop();
+		myStack.printStack();
+		assertEquals(mySecondNode, popedNode2);
+		INode<Integer> popedNode3 = myStack.pop();
+		assertEquals(myFirstNode, popedNode3);
 	}
 }
